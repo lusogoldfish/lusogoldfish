b9579878a9f11c8e4bb82b86887b69c89ae533b2
LusoGoldFish <lusogoldfish12@gmail.com>
2025-03-13 21:25:16 +0000

    pagina principal e tema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,67 +5,72 @@
 
         <div id="hero-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
 
-          <div class="carousel-item active">
-            <img src="assets/img/hero-carousel/hero-carousel-1.jpg" alt="">
-            <div class="carousel-container">
-              <h2>Bem vindo ao <span>LusoGoldFish</span></h2>
-              <p>Peixes Ornamentais de Qualidade para o Seu Aquário! <br>Aqui na LusoGoldFish, oferecemos uma seleção de peixes ornamentais,
-                aquários e acessórios para transformar qualquer espaço num ambiente aquático deslumbrante.</p>
-              <a href="about.html" class="btn-get-started">Get Started</a>
-            </div>
-          </div><!-- End Carousel Item -->
+            <div class="carousel-item active">
+                <img src="eterna/img/hero-carousel/hero-carousel-1.jpg" alt="">
+                <div class="carousel-container">
+                    <h2>Bem vindo ao <span>LusoGoldFish</span></h2>
+                    <p>Peixes Ornamentais de Qualidade para o Seu Aquário! <br>Aqui na LusoGoldFish, oferecemos uma
+                        seleção de peixes ornamentais,
+                        aquários e acessórios para transformar qualquer espaço num ambiente aquático deslumbrante.</p>
+                    <a href="/" class="btn-get-started"><i class="bi bi-search"></i> Descobre Agora!</a>
+                </div>
+            </div><!-- End Carousel Item -->
 
-          <div class="carousel-item">
-            <img src="assets/img/hero-carousel/hero-carousel-2.jpg" alt="">
-            <div class="carousel-container">
-              <h6>adad</h5>
+            <div class="carousel-item">
+                <img src="eterna/img/hero-carousel/hero-carousel-2.jpg" alt="">
+                <div class="carousel-container">
+                    <h2>Dá <span>Vida</span> ao Teu <span>Aquário!</span></h2>
+                    <p>Descobre uma grande variedade de peixes ornamentais e os melhores acessórios para manter o teu
+                        aquário vibrante e saudável.</p>
+                    <a href="/" class="btn-get-started"><i class="bi bi-search"></i> Descobre Agora!</a>
+                </div>
+            </div><!-- End Carousel Item -->
 
-              <a href="about.html" class="btn-get-started">Get Started</a>
-            </div>
-          </div><!-- End Carousel Item -->
+            <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+            </a>
 
-          <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-          </a>
-
-          <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-          </a>
+            <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+            </a>
 
         </div>
 
         <div class="featured container">
 
-          <div class="row gy-4">
+            <div class="row gy-4">
 
-            <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="100">
-              <div class="featured-item position-relative">
-                <div class="icon"><i class="bi bi-bounding-box-circles icon"></i></div>
-                <h4><a href="" class="stretched-link">Sed ut perspici</a></h4>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-              </div>
-            </div><!-- End Featured Item -->
+                <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="100">
+                    <div class="featured-item position-relative">
+                        <div class="icon"><i class="bi bi-award"></i></div>
+                        <h4><a href="" class="stretched-link"> Qualidade Garantida</a></h4>
+                        <p>Trabalhamos com fornecedores certificados para oferecer produtos de alta qualidade.</p>
+                    </div>
+                </div><!-- End Featured Item -->
 
-            <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="200">
-              <div class="featured-item position-relative">
-                <div class="icon"><i class="bi bi-calendar4-week icon"></i></div>
-                <h4><a href="" class="stretched-link">Magni Dolores</a></h4>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-              </div>
-            </div><!-- End Featured Item -->
+                <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="200">
+                    <div class="featured-item position-relative">
+                        <div class="icon"><i class="bi bi-people"></i></div>
+                        <h4><a href="" class="stretched-link">Suporte Especializado</a></h4>
+                        <p> Precisas de ajuda? A nossa equipa está pronta para te aconselhar na escolha ideal.</p>
+                    </div>
+                </div><!-- End Featured Item -->
 
-            <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="300">
-              <div class="featured-item position-relative">
-                <div class="icon"><i class="bi bi-broadcast icon"></i></div>
-                <h4><a href="" class="stretched-link">Nemo Enim</a></h4>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-              </div>
-            </div><!-- End Featured Item -->
+                <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="300">
+                    <div class="featured-item position-relative">
+                        <div class="icon"><i class="bi bi-box-seam"></i></div>
+                        <h4><a href="" class="stretched-link">Entregas Rápidas</a></h4>
+                        <p>Recebe os teus produtos com rapidez e segurança em qualquer ponto do país.</p>
+                    </div>
+                </div><!-- End Featured Item -->
 
-          </div>
-
+            </div>
         </div>
 
-      </section><!-- /Hero Section -->
+    </section><!-- /Hero Section -->
+
+
+
+
 
 </x-eterna-layout>
