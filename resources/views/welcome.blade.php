1834f815f79d15fba25f25a32c1e00ffaa38dee1
LusoGoldFish <lusogoldfish12@gmail.com>
2025-03-14 16:11:02 +0000

    Pagina principal 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,36 @@
     </section><!-- /Hero Section -->
 
 
+    <div class="bg-white">
+  <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Os Nossos Clientes Adoram!</h2>
+
+    <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+        <img src="eterna/img/produtos/peixes/beta.jpg" alt="Peixe Beta" class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80">
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Peixe Beta
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-red-500">Vermelho</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">35€</p>
+        </div>
+      </div>
+
+      <!-- More products... -->
+
+      
+    </div>
+  </div>
+</div>
+
+
+
 
 
 
